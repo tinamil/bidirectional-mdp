76fc7054d21b25e0f86de797c760027a3545cb02
John Pavlik <john.a.pavlik@gmail.com>
2018-11-22 23:40:17 -0600

    Setup racetrack problem

---
+++ main.py
@@ -0,0 +1,120 @@
+from racetrack import Racetrack
+import pygame
+import sys
+import argparse
+import time
+
+import pygame.locals
+
+
+class Application:
+    def __init__(self, filename: str, scale=20, fps=30):
+        self.running = True
+        self.displaySurface = None
+        self.scale = scale
+        self.fps = fps
+        self.windowTitle = "Racetrack MDP: " + filename
+
+        self.track = Racetrack(filename)
+        self.dim = self.track.get_dimensions()
+
+        self.windowHeight = self.dim[0] * self.scale
+        self.windowWidth = self.dim[1] * self.scale
+
+        self.blockSizeX = int(self.windowWidth / self.dim[1])
+        self.blockSizeY = int(self.windowHeight / self.dim[0])
+
+    def execute(self, searchMethod, save):
+
+        #path, statesExplored = search(self.maze, searchMethod)
+
+        pygame.init()
+        self.displaySurface = pygame.display.set_mode((self.windowWidth, self.windowHeight), pygame.HWSURFACE)
+        self.displaySurface.fill((255, 255, 255))
+        pygame.display.flip()
+        pygame.display.set_caption(self.windowTitle)
+
+        print("Results")
+        #print("Path Length:", len(path))
+        #print("States Explored:", statesExplored)
+        #self.drawPolicy(path)
+
+        self.drawTrack()
+        self.drawStart()
+        self.drawObjective()
+
+        pygame.display.flip()
+        if save is not None:
+            pygame.image.save(self.displaySurface, save)
+            self.running = False
+
+        clock = pygame.time.Clock()
+
+        while self.running:
+            pygame.event.pump()
+            keys = pygame.key.get_pressed()
+            clock.tick(self.fps)
+
+            if any(keys):
+                raise SystemExit
+
+    def getColor(self, pathLength, index):
+        step = 255 / pathLength
+        green = index * step
+        red = 255 - green
+        return red, green, 0
+
+    def drawPolicy(self, path):
+        for p in range(len(path)):
+            color = self.getColor(len(path), p)
+            self.drawSquare(path[p][0], path[p][1], color)
+
+    # Simple wrapper for drawing a wall as a rectangle
+    def drawWall(self, row, col):
+        pygame.draw.rect(self.displaySurface, (0, 0, 0), (col * self.blockSizeX, row * self.blockSizeY, self.blockSizeX, self.blockSizeY), 0)
+
+    # Simple wrapper for drawing a circle
+    def drawCircle(self, row, col, color, radius=None):
+        if radius is None:
+            radius = min(self.blockSizeX, self.blockSizeY) / 4
+        pygame.draw.circle(self.displaySurface, color, (int(col * self.blockSizeX + self.blockSizeX / 2), int(row * self.blockSizeY + self.blockSizeY / 2)), int(radius))
+
+    def drawSquare(self, row, col, color):
+        pygame.draw.rect(self.displaySurface, color, (col * self.blockSizeX, row * self.blockSizeY, self.blockSizeX, self.blockSizeY), 0)
+
+    # Draws the objectives to the display context
+    def drawObjective(self):
+        for obj in self.track.get_objectives():
+            self.drawCircle(obj[0], obj[1], (0, 0, 0))
+
+    # Draws start location of path
+    def drawStart(self):
+        for x, y in self.track.get_start():
+            pygame.draw.rect(self.displaySurface, (0, 0, 255), (y * self.blockSizeX + self.blockSizeX / 4, x * self.blockSizeY + self.blockSizeY / 4, self.blockSizeX * 0.5, self.blockSizeY * 0.5), 0)
+
+    # Draws the full maze to the display context
+    def drawTrack(self):
+        for row in range(self.dim[0]):
+            for col in range(self.dim[1]):
+                if self.track.is_wall(row, col):
+                    self.drawWall(row, col)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--filename', default="small_track.txt", type=str,
+                        help='path to racetrack file - default ')
+    parser.add_argument('--method', dest="search", type=str, default="dblao",
+                        choices=["lao", "blao", "dblao"],
+                        help='search method - default dblao')
+    parser.add_argument('--scale', dest="scale", type=int, default=20,
+                        help='scale - default: 20')
+    parser.add_argument('--fps', dest="fps", type=int, default=30,
+                        help='fps for the display - default 30')
+    parser.add_argument('--save', dest="save", type=str, default=None,
+                        help='save output to image file - default not saved')
+
+    args = parser.parse_args()
+    app = Application(args.filename, args.scale, args.fps)
+    app.execute(args.search, args.save)
---
+++ racetrack.py
@@ -0,0 +1,136 @@
+"""A race track of any shape is drawn on graph paper, with a starting line at one end and a finish line at the other consisting of designated squares. Each square within the
+boundary of the track is a possible location of the car. At the start of each of a sequence of trials, the car is placed on the starting line at
+a random position, and moves are made in which the car attempts to move down the track toward the finish line. Acceleration and deceleration are simulated as follows. If
+in the previous move the car moved h squares horizontally and u squares vertically, then the present move can be h’ squares vertically and v’ squares horizontally, where
+the difference between h’ and h is - 1, 0, or 1, and the difference between v’ and v is - 1, 0, or 1. This means that the car can maintain its speed in either dimension, or it
+can slow down or speed up in either dimension by one square per move. If the car hits the track boundary, we move it back to a random position on the starting line, reduce
+its velocity to zero (i.e., h’ - h and U’ - c’ are considered to be zero), and continue the trial. The objective is to learn to control the car so that it crosses the finish line in as
+few moves as possible.
+
+In addition to the difficulty of discovering faster ways to reach the finish line, it is very easy for the car to gather too much speed to negotiate the track’s curves. To make
+matters worse, we introduce a random factor into the problem. With a probability p, the actual accelerations or decelerations at a move are zero independently of the intended
+accelerations or decelerations. Thus, 1 -p is the probability that the controller’s intended actions are executed. One might think of this as simulating driving on a track that is
+unpredictably slippery so that sometimes braking and throttling up have no effect on the car’s velocity.  -Barto et al. - 1995 - Learning to act using real-time dynamic programming
+
+Current state is a quadruple tuple of integers (x, y, x', y') representing horizontal and vertical position and horizontal and vertical velocity.  Valid actions at time t are 9 different velocity
+changes in the range -1 to +1: i.e. {(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 0), (0, 1), (1, -1), (1, 0), or (1, 1)}
+
+Velocity at step t+1 is x' + (-1 to 1), position is position + new velocity with probability p
+Velocity changes are ignored (track slippage) and position is position + old velocity with probability (1-p)
+
+If the line connecting the old and new position in 2D space intersects a track boundary then a collision occurs.  The velocity is set to 0 and the car is moved
+back to the starting position.
+
+If the finish line is crossed then an absorbing goal state is reached and the MDP is terminated.
+
+All actions are unit cost, i.e. c(state) = 1
+
+Only policies that are guaranteed to cross the finish line are considered to avoid the need to use discounting to prevent infinite loops.  Therefore, the infinite horizon cost is the
+expected number of moves for the car to cross the finish line from state i.  An optimal policy minimizes the number of expected moves to reach the finish line.
+
+"""
+
+import re
+import copy
+import numpy as np
+from typing import Tuple, List
+
+Position = Tuple[int, int]
+Velocity = Tuple[int, int]
+Dimension = Tuple[int, int]
+
+
+class Racetrack:
+    def __init__(self, filename: str) -> None:
+        '''
+        Given a file will read in and initialize the racetrack.  File must have exactly one start char and at least one reachable goal character
+        :param filename:
+        '''
+        self.__wallChar = '%'
+        self.__startChar = 'P'
+        self.__objectiveChar = '.'
+        self.__start: List[Position] = []
+        self.__objective: List[Position] = []
+
+        with open(filename) as f:
+            lines = f.readlines()
+
+        """Strip out any lines that only consist of whitespace"""
+        lines = list(filter(lambda x: not re.match(r'^\s*$', x), lines))
+        """Remove all newlines"""
+        lines = [list(line.strip('\n')) for line in lines]
+
+        self.track = np.array(lines)
+
+        self.rows = self.track.shape[0]
+        self.cols = self.track.shape[1]
+
+        for x in range(self.rows):
+            for y in range(self.cols):
+                if self.track[x][y] == self.__startChar:
+                    self.__start.append((x, y))
+                elif self.track[x][y] == self.__objectiveChar:
+                    self.__objective.append((x, y))
+
+    def is_wall(self, x: int, y: int) -> bool:
+        return self.track[x][y] == self.__wallChar
+
+    def is_objective(self, x: int, y: int) -> bool:
+        return (x, y) in self.__objective
+
+    def get_objectives(self) -> List[Position]:
+        return copy.deepcopy(self.__objective)
+
+    def get_start(self) -> List[Position]:
+        return self.__start
+
+    def get_dimensions(self) -> Dimension:
+        return self.rows, self.cols
+
+    def __is_collision(self, x0: int, y0: int, x1: int, y1: int) -> bool:
+        nodes = self.raytrace(x0, y0, x1, y1)
+        for x, y in nodes:
+            if self.is_wall(x, y):
+                return True
+        return False
+
+    def __is_goal(self, x0: int, y0: int, x1: int, y1: int) -> bool:
+        nodes = self.raytrace(x0, y0, x1, y1)
+        for x, y in nodes:
+            if self.is_objective(x, y):
+                return True
+        return False
+
+    @staticmethod
+    def raytrace(x0: int, y0: int, x1: int, y1: int) -> List[Position]:
+        """
+        Retuns a list of all positions (x,y) on a line between (x0, y0) and (x1, y1), inclusive
+        http://playtechs.blogspot.com/2007/03/raytracing-on-grid.html
+        """
+        dx = abs(x1 - x0)
+        dy = abs(y1 - y0)
+        x = x0
+        y = y0
+        n = 1 + dx + dy
+        if x1 > x0:
+            x_inc = 1
+        else:
+            x_inc = -1
+        if y1 > y0:
+            y_inc = 1
+        else:
+            y_inc = -1
+        error = dx - dy
+        dx *= 2
+        dy *= 2
+        tiles_visited = []
+        for i in range(n, 0):
+            tiles_visited.append((x, y))
+
+            if error > 0:
+                x += x_inc
+                error -= dy
+            else:
+                y += y_inc
+                error += dx
+        return tiles_visited
